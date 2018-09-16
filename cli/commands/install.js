18447dd7e0d7760c815092755c2018ad5a069e8d
sunyuming <sunyuming@quarkioe.com>
2018-09-16 15:40:04 +0800

    0.0.1.1

--- cli/commands/install.js
+++ cli/commands/install.js
@@ -1,9 +1,12 @@
 const options = require('../../lib/options');
+const shell = require('shelljs');
 
 module.exports.command = (program) => {
   const programs = program
     .command('install')
     .alias('i')
     .description('Install a Quarkioe UI package using npm.')
-    .action(() => {});
+    .action(() => {
+      shell.exec('mkdir ceshi');
+    });
 };
